34e0c2488a9320eb114286d8653d99095741151f
AlainNtumba <50066005@mylife.unisa.ac.za>
2026-04-24 17:43:53 +0200

    Implement WebAssembly progressive App

--- AdimSystem/Components/Pages/ncd/Login.razor.cs
+++ AdimSystem/Components/Pages/ncd/Login.razor.cs
@@ -1,10 +1,66 @@
 using AdimSystem.Components.Shared;
-using ChandorProject.Shared.Models;
+using AdimSystem.Interfaces;
+using ChandorProject.Shared.DTOs.User;
+using Microsoft.AspNetCore.Components;
 
 namespace AdimSystem.Components.Pages.ncd;
 
 public partial class Login
 {
+    [Inject] public IAuthService _auth { get; set; } = default!;
+    [Inject] public NavigationManager _nav { get; set; } = default!;
     public CustomFormValidator FormValidator { get; set; } = new();
-    public LoginRequest User { get; set; } = new();
+    public LoginRequestDto User { get; set; } = new();
+    public Dictionary<string, List<string>> Errors { get; set; } = new();
+    public string LoginForm { get; set; } = "login-form";
+    public string ErrorMsg { get; set; } = string.Empty;
+    public bool IsProcessing { get; set; } = false;
+
+    public async Task AuthenticateUser()
+    {
+        ErrorMsg = "";
+        Errors.Clear();
+        FormValidator.ClearFormErrors();
+
+        if (string.IsNullOrEmpty(User.Username) || string.IsNullOrEmpty(User.Password))
+        {
+            ErrorMsg = "Invalid username or password";
+            Errors.Add(nameof(User.Username), new List<string> { "" });
+            Errors.Add(nameof(User.Password), new List<string> { "" });
+            FormValidator.DisplayFormErrors(Errors);
+            StateHasChanged();
+        }
+        else
+        {
+            IsProcessing = true;
+
+            var authUser = await _auth.LoginAsync(User);
+
+            IsProcessing = false;
+
+            if (authUser != null)
+            {
+                if (authUser.Success)
+                {
+                    _nav.NavigateTo("/");
+                }
+                else
+                {
+                    ErrorMsg = authUser.Message is not null ? authUser.Message : "Failed authentication";
+                    Errors.Add(nameof(User.Username), new List<string> { "" });
+                    Errors.Add(nameof(User.Password), new List<string> { "" });
+                    FormValidator.DisplayFormErrors(Errors);
+                    StateHasChanged();
+                }
+            }
+            else
+            {
+                ErrorMsg = "Failed authentication";
+                Errors.Add(nameof(User.Username), new List<string> { "" });
+                Errors.Add(nameof(User.Password), new List<string> { "" });
+                FormValidator.DisplayFormErrors(Errors);
+                StateHasChanged();
+            }
+        }
+    }
 }
---
+++ ChandorAdmin/ChandorAdmin/Program.cs
@@ -0,0 +1,15 @@
+﻿using ChandorAdmin;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JHaF5cWWdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdlWXtednVVRWddWUN3V0dWYEo=");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
